24ee182c348d51aa81851cee48c617d467045559
Muhammad Khurram Aetzaz Qaiser <khurram.programming@gmail.com>
2022-09-24 04:20:12 -0700

    Calculate_Distance_Between_Two_Points.py

---
+++ Chapter_2/Calculate_Distance_Between_Two_Points.py
@@ -0,0 +1,11 @@
+"""
+Write a python program to calculate distance between two points by using distance formula.
+Distance Formula = ((x2-x1)^2+(y2-y1)^2)^(1/2)
+"""
+
+x1 = 2
+y1 = 2
+x2 = 4
+y2 = 4
+distance = (((x2-x1)**2)+((y2-y1)**2))**(1/2)
+print("The distance between two points is:",distance)
