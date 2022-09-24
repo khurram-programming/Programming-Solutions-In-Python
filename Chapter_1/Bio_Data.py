4385432465277f1fbe5d0935c719b6721073896c
Muhammad Khurram Aetzaz Qaiser <khurram.programming@gmail.com>
2022-09-24 00:38:42 -0700

    Create Bio_Data.py

---
+++ Chapter_1/Bio_Data.py
@@ -0,0 +1,9 @@
+"""
+Write a python program that prints your Bio_Data on the screen.
+"""
+# You can enter your details
+print("Name")
+print("Education")
+print("Phone number")
+print("E-mail address")
+print("Home address")
