0cc624830352e831f390959458829b95d2b55065
Muhammad Khurram Aetzaz Qaiser <khurram.programming@gmail.com>
2022-09-24 01:43:46 -0700

    Create Calculate_The_Area_Of_Circle.py

---
+++ Chapter_2/Calculate_The_Area_Of_Circle.py
@@ -0,0 +1,10 @@
+"""
+Write a python program that calculates the area of a circle.
+Formula Area = 𝜋r^2
+(Consider r = 100)
+"""
+
+r = 100
+pi = 3.14
+area = pi*(r**2)
+print("The area of circle is: ", area," units")
