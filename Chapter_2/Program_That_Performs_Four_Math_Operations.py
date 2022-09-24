660cb96d70291fbc41c51cebd9669c3189a27ebe
Muhammad Khurram Aetzaz Qaiser <khurram.programming@gmail.com>
2022-09-24 02:03:03 -0700

    Program_That_Performs_Four_Math_Operations.py
    Create Program_That_Performs_Four_Math_Operations.py

---
+++ Chapter_2/Program_That_Performs_Four_Math_Operations.py
@@ -0,0 +1,17 @@
+"""
+Write a python program that performs the following four math operations.
+And print their result on the screen.
+a. 100 + 4
+b. 100 - 4
+c. 100 * 4
+d. 100 / 4
+"""
+
+a = 100 + 4
+b = 100 - 4
+c = 100 * 4
+d = 100 / 4
+print("100 + 4 =",a)
+print("100 - 4 =",b)
+print("100 * 4 =",c)
+print("100 / 4 =",d)
